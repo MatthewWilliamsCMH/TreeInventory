b93193be1b830cf9febf2d815dc2b3bf0659038e
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-12-28 10:35:30 -0500

    more progress on photos; uploading multiple times; still throwing error

--- server/server.js
+++ server/server.js
@@ -1,15 +1,45 @@
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
+const multer = require("multer");
+const path = require("path")
+const cors = require("cors");
+require("dotenv").config();
+
+const fs = require('fs');
+const uploadsDir = path.join(__dirname, 'public', 'uploads');
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true });
+}
 const { connectDB } = require("./config/connection");
 const { typeDefs } = require("./schemas/typeDefs");
 const { resolvers } = require("./schemas/resolvers");
-const path = require("path") 
-const cors = require("cors");
-require("dotenv").config;
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+const storage = multer.diskStorage({
+  destination: (req, photo, cb) => {
+    cb(null, path.join(__dirname, 'public', 'uploads')); // Save to /public/uploads
+  },
+  filename: (req, photo, cb) => {
+    cb(null, Date.now() + path.extname(photo.originalname)); // Rename the file to avoid conflicts
+  }
+});
+
+// Initialize multer with the storage configuration
+const upload = multer({ storage: storage });
+
+app.post('/uploads', upload.single('photo'), (req, res) => {
+  console.log('Incoming file:', req.photo);  // Log the file information
+  console.log('Request body:', req.body);  // Log the body (should contain 'photo')
+
+  if (!req.photo) {
+    return res.status(400).send({ message: 'No photo uploaded' });
+  }
+  res.status(200).send({ message: 'Photo uploaded successfully', photo: req.photo });
+});
 
 //enable cors to allow front-end and back-end ports to connect in any enviroment
 app.use(cors());
