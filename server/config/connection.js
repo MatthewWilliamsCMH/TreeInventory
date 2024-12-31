9a27777d0aaa5071069215b175a91699f4bf995f
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-12-31 18:57:16 -0500

    changed back to standard pins for now; circleMarker not draggable

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/treeInventoryDB');
