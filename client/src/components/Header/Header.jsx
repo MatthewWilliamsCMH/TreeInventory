2ad45c7ef48a23d11c8b84922c6e0900b98de69e
Matthew Williams <mattthewwilliamscmh@icloud.com>
2025-07-25 12:56:01 -0400

    attempt to fix head wrapping on small screens

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   //render component
   return (
     <header id='header' className={styles.header} >
-      <h1>Summit Chase Tree Inventory</h1>
+      <h1 className = 'fs-5 fs-sm-4 fs-md-3 fs-lg-2 fs-xl-1'>Summit Chase Tree Inventory</h1>
     </header>
   );
 }
