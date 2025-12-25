6720442a1166d5678a3cb6827209023a89b0716c
Matthew Williams <mattthewwilliamscmh@icloud.com>
2025-12-25 10:24:25 -0500

    still trying to get rebuilt project to conenct to db; test file set up for graphql.js

--- api/graphql.js
+++ api/graphql.js
@@ -1,15 +1,20 @@
-// server/api/graphql.js
-const { createApp } = require('../server/createApp');
+// // server/api/graphql.js
+// const { createApp } = require('../server/createApp');
 
-let appPromise = null;
+// let appPromise = null;
 
-// For serverless, we reuse the same instance if possible
-module.exports = async (req, res) => {
-  if (!appPromise) {
-    appPromise = createApp();
-  }
-  const app = await appPromise;
+// // For serverless, we reuse the same instance if possible
+// module.exports = async (req, res) => {
+//   if (!appPromise) {
+//     appPromise = createApp();
+//   }
+//   const app = await appPromise;
 
-  // Express apps can be called directly as functions in Vercel
-  app(req, res);
-};
+//   // Express apps can be called directly as functions in Vercel
+//   app(req, res);
+// };
+
+// root/api/graphql.js
+export default function handler(req, res) {
+  res.status(200).json({ ok: true });
+}
